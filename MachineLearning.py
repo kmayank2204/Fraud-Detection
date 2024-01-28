56c3ea9e6fa176dee8c7f455ddb83bfb6f86e295
kmayank2204 <1420019@kiit.ac.in>
2024-01-28 22:14:36 +0530

    Post this file into new branch

--- MachineLearning.py
+++ MachineLearning.py
@@ -1,1 +1,9 @@
 print("This is posting into the new branch")
+
+def modelFunction1():
+    print("adding code to MachineLearning file")
+
+def modelFunction2():
+    print("adding code 2 to MachineLearning file")
+
+
